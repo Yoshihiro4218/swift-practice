bfd9f107cf38e044ac4f512a927e477149b0a5f5
yoshihiro <y.kadono4218@gmail.com>
2020-06-07 19:31:30 +0900

    first commit

--- swift-practice/ViewController.swift
+++ swift-practice/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapButton(_ sender: Any) {
+        textLabel.text = "ボタンがタップされたよ";
+    }
+    
+    @IBAction func returnButton(_ sender: Any) {
+        textLabel.text = "ざわ...ざわ...";
+    }
 }
 
